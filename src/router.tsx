3fb20f24f8e772d088eb939386cae4d9a58a88f9
Anchovia <hodolye@naver.com>
2025-11-01 17:03:23 +0900

    feat: React Router v6 기반 전체 페이지 라우팅 뼈대 구현, chore: 폼 유효성 검사를 위한 zod, react-hook-form 의존성 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import App from "./App";
-import Home from "./components/home";
+import AdminHome from "./pages/AdminHome";
+import AdminReserveManagement from "./pages/AdminReserveManagement";
+import AdminUserManagement from "./pages/AdminUserManagement";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
+import Register from "./pages/Register";
+import Reserve from "./pages/Reserve";
 
 // 라우트 정의
 export const router = createBrowserRouter([
@@ -9,9 +16,46 @@ export const router = createBrowserRouter([
         element: <App />, // 공통 레이아웃
         // errorElement: <ErrorPage />, // 에러 페이지
         children: [
+            // 일반 사용자 페이지
             {
-                index: true, // path: '/'와 동일한 경로
+                index: true, // path: '/'
                 element: <Home />,
+            },
+            {
+                path: "login",
+                element: <Login />,
+            },
+            {
+                path: "register",
+                element: <Register />,
+            },
+            {
+                path: "reserve",
+                element: <Reserve />,
+            },
+            {
+                path: "my_page",
+                element: <MyPage />,
+            },
+
+            // 관리자 페이지
+            {
+                path: "admin",
+                element: <Outlet />,
+                children: [
+                    {
+                        index: true, // path: '/admin'
+                        element: <AdminHome />,
+                    },
+                    {
+                        path: "user", // path: '/admin/user'
+                        element: <AdminUserManagement />,
+                    },
+                    {
+                        path: "reserve", // path: '/admin/reserve'
+                        element: <AdminReserveManagement />,
+                    },
+                ],
             },
         ],
     },
