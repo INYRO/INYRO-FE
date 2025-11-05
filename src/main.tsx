7c37437f69f1bee8d9536f03d13e8dc5f465b2f8
ByeongHun Jeon <carolsuki7@gmail.com>
2025-11-05 14:37:25 +0900

    chore: 개발 환경 설정 완료 (절대 경로, VSCode 설정, 상대 경로 제거), docs: README에 누락된 내용 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { store } from "./store/store";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    resolve: {
+        // alias 설정(절대 경로)
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
