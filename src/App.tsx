1d3dfab21c27c66bbe8eeefe19c7e30023db4303
ByeongHun Jeon <carolsuki7@gmail.com>
2025-11-05 11:19:38 +0900

    feat: Redux 및 모달 기능 추가 - package.json 및 package-lock.json에 @reduxjs/toolkit 및 react-redux 추가, - Modal 컴포넌트 생성 및 App.tsx에 통합, - modalSlice.ts 및 store.ts 파일 생성하여 상태  관리 구현, - hooks.ts 파일 추가하여 useAppDispatch 및 useAppSelector 정의, fix: eslint config가 작동하지 않던 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Modal from "./components/common/modal";
 
 function App() {
     return (
-        <main className="layout">
-            <Outlet />
-        </main>
+        <>
+            <main className="layout">
+                <Outlet />
+            </main>
+            <Modal />
+        </>
     );
 }
 
---
+++ src/store/modalSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+// modal state Type
+interface ModalState {
+    isOpen: boolean;
+    modalType: string | null; // 어떤 모달을 띄울지 식별 (ex: 'login', 'confirmDelete')
+}
+
+// state의 초기값 설정
+const initialState: ModalState = {
+    isOpen: false,
+    modalType: null,
+};
+
+export const modalSlice = createSlice({
+    name: "modal",
+    initialState,
+    // state를 변경하는 함수(리듀서)들을 reducers 객체 안에 정의
+    reducers: {
+        // openModal 액션
+        // PayloadAction<string>은 action.payload의 타입이 string임을 의미
+        openModal: (state, action: PayloadAction<string>) => {
+            state.isOpen = true;
+            state.modalType = action.payload; // payload로 받은 모달 타입을 state에 저장
+        },
+        // closeModal 액션
+        closeModal: (state) => {
+            state.isOpen = false;
+            state.modalType = null;
+        },
+    },
+});
+
+// 생성된 액션 생성자(action creators)들을 export
+// 컴포넌트에서 이 액션들을 dispatch(실행)할 수 있음
+export const { openModal, closeModal } = modalSlice.actions;
+
+// store.ts에서 reducer를 등록할 수 있도록 default로 export
+export default modalSlice.reducer;
