311bb044bf844a96b9d97719d219e0202f70b597
Anchovia <hodolye@naver.com>
2026-02-20 09:53:15 +0900

    refactor: router.tsx의 ProtectedRoute 사용 위치 변경 및 ProtectedAdminRoute의 코드 리팩토링, docs: router.tsx에 주석 추가

--- src/components/route/ProtectedAdminRoute.tsx
+++ src/components/route/ProtectedAdminRoute.tsx
@@ -12,7 +12,7 @@ export const ProtectedAdminRoute = ({
     const location = useLocation();
 
     // 부팅(인증 복구) 중이면 판단하지 말고 잠깐 대기(깜빡임 방지)
-    if (!authInitialized && !user) {
+    if (!authInitialized) {
         return (
             <div className="min-h-screen flex items-center justify-center">
                 로딩중...
@@ -20,9 +20,13 @@ export const ProtectedAdminRoute = ({
         );
     }
 
-    // 부팅 이후 로그인 상태가 아니거나, Bossisme가 아니면 메인 페이지로 이동
-    if (!isLogin || user?.sno !== "Bossisme") {
-        return <Navigate to="/" replace state={{ from: location }} />;
+    // 부팅 이후 로그인 상태가 아니면 로그인 페이지로 이동
+    if (!isLogin) {
+        return <Navigate to="/login" replace state={{ from: location }} />;
+    }
+    // Bossisme가 아니면 메인 페이지로 이동
+    if (user?.sno !== "Bossisme") {
+        return <Navigate to="/" replace />;
     }
 
     return children;
