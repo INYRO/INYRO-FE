29ab1910545c94883553b7557c1669ffc620550b
Anchovia <hodolye@naver.com>
2026-03-03 23:08:52 +0900

    refactor: AdminUserManagement 리팩토링

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,7 +1,9 @@
 import type { ReservationListResponse } from "@/types/reservation";
 import axiosInstance from "./axiosInstance";
 import type { ApiResponse } from "@/types/api";
+import type { MemberListResponse } from "@/types/member";
 
+// 어드민 예약 목록 불러오기 api
 export const getAdminReservationList = async () => {
     const response = await axiosInstance.get<ReservationListResponse>(
         "/admin/reservations"
@@ -9,6 +11,7 @@ export const getAdminReservationList = async () => {
     return response.data;
 };
 
+// 어드민 예약 삭제 api
 export interface DeleteReservationPayload {
     reservationIdList: number[];
 }
@@ -17,6 +20,40 @@ export const deleteAdminReservationApi = async (
 ) => {
     const response = await axiosInstance.delete<ApiResponse<string>>(
         "/admin/reservations",
+        { data }
+    );
+    return response.data;
+};
+
+// 어드민 유저 리스트 불러오기 api
+export interface GetAdminUserListPayload {
+    sortType: string;
+    order: string;
+    pages: number;
+    sizes: number;
+}
+export const getAdminUserList = async ({
+    sortType,
+    order,
+    pages,
+    sizes,
+}: GetAdminUserListPayload) => {
+    const response = await axiosInstance.get<MemberListResponse>(
+        "/admin/members",
+        {
+            params: { sortType, order, page: pages, size: sizes },
+        }
+    );
+    return response.data;
+};
+
+// 어드민 유저 삭제 api
+export interface DeleteUserPayload {
+    snoList: string[];
+}
+export const deleteAdminUserApi = async (data: DeleteUserPayload) => {
+    const response = await axiosInstance.delete<ApiResponse<string>>(
+        "/admin/members/withdrawal",
         { data }
     );
     return response.data;
